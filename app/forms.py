4d4f7b245def150ff1d84afeb731df7e16dc0c32
hemalatha164 <hemalatha4004@gmail.com>
2023-04-25 21:20:45 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 def check_for_a(value):
     if value[0].lower()=='a':
-        raise forms.ValidationError('name should start with a')
+        raise forms.ValidationError('name should not start with a')
 
 
 
@@ -10,3 +10,11 @@ def check_for_a(value):
 class StudentForm(forms.Form):
     name=forms.CharField(max_length=100,required=True,validators=[check_for_a])
     age=forms.IntegerField()
+    Email=forms.EmailField()
+    re_enter_Email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher'] 
+        if len(bot)>0:
+           raise forms.ValidationError('bot')
